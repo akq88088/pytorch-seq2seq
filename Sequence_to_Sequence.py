657d88ed9158b1456e7f8fc1ad3480229ad945c4
LIANG-TING-CHEN <54851652+LIANG-TING-CHEN@users.noreply.github.com>
2022-03-16 22:49:13 +0800

    new

--- Sequence_to_Sequence.py
+++ Sequence_to_Sequence.py
@@ -31,10 +31,6 @@ spacy_zh = spacy.load('zh_core_web_sm')
 spacy_de = spacy.load('de_core_news_sm')
 spacy_en = spacy.load('en_core_web_sm')
 
-# Next, we create the tokenizer functions. These can be passed to torchtext and will take in the sentence as a string and return the sentence as a list of tokens.
-# 
-# In the paper we are implementing, they find it beneficial to reverse the order of the input which they believe "introduces many short term dependencies in the data that make the optimization problem much easier". We copy this by reversing the German sentence after it has been transformed into a list of tokens.
-
 def tokenize_de(text):
     """
     Tokenizes German text from a string into a list of strings (tokens) and reverses it
@@ -50,10 +46,6 @@ def tokenize_en(text):
 def tokenize_zh(text):
     return [tok.text for tok in spacy_zh.tokenizer(text)]
 
-# torchtext's `Field`s handle how data should be processed. All of the possible arguments are detailed [here](https://github.com/pytorch/text/blob/master/torchtext/data/field.py#L61). 
-# 
-# We set the `tokenize` argument to the correct tokenization function for each, with German being the `SRC` (source) field and English being the `TRG` (target) field. The field also appends the "start of sequence" and "end of sequence" tokens via the `init_token` and `eos_token` arguments, and converts all words to lowercase.
-
 SRC = Field(tokenize = tokenize_de, 
             init_token = '<sos>', 
             eos_token = '<eos>', 
@@ -64,64 +56,18 @@ TRG = Field(tokenize = tokenize_en,
             eos_token = '<eos>', 
             lower = True)
 
-
-# Next, we download and load the train, validation and test data. 
-# 
-# The dataset we'll be using is the [Multi30k dataset](https://github.com/multi30k/dataset). This is a dataset with ~30,000 parallel English, German and French sentences, each with ~12 words per sentence. 
-# 
-# `exts` specifies which languages to use as the source and target (source goes first) and `fields` specifies which field to use for the source and target.
-
-
-
 train_data, valid_data, test_data = Multi30k.splits(exts = ('.de', '.en'), 
                                                     fields = (SRC, TRG))
-
-
-# We can double check that we've loaded the right number of examples:
-
-
 
 print(f"Number of training examples: {len(train_data.examples)}")
 print(f"Number of validation examples: {len(valid_data.examples)}")
 print(f"Number of testing examples: {len(test_data.examples)}")
 
-
-# We can also print out an example, making sure the source sentence is reversed:
-
-
-
-print(vars(train_data.examples[0]))
-
-
-# The period is at the beginning of the German (src) sentence, so it looks like the sentence has been correctly reversed.
-# 
-# Next, we'll build the *vocabulary* for the source and target languages. The vocabulary is used to associate each unique token with an index (an integer). The vocabularies of the source and target languages are distinct.
-# 
-# Using the `min_freq` argument, we only allow tokens that appear at least 2 times to appear in our vocabulary. Tokens that appear only once are converted into an `<unk>` (unknown) token.
-# 
-# It is important to note that our vocabulary should only be built from the training set and not the validation/test set. This prevents "information leakage" into our model, giving us artifically inflated validation/test scores.
-
-
-
 SRC.build_vocab(train_data, min_freq = 2)
 TRG.build_vocab(train_data, min_freq = 2)
 
-
-
-
 print(f"Unique tokens in source (de) vocabulary: {len(SRC.vocab)}")
 print(f"Unique tokens in target (en) vocabulary: {len(TRG.vocab)}")
-
-
-# The final step of preparing the data is to create the iterators. These can be iterated on to return a batch of data which will have a `src` attribute (the PyTorch tensors containing a batch of numericalized source sentences) and a `trg` attribute (the PyTorch tensors containing a batch of numericalized target sentences). Numericalized is just a fancy way of saying they have been converted from a sequence of readable tokens to a sequence of corresponding indexes, using the vocabulary. 
-# 
-# We also need to define a `torch.device`. This is used to tell torchText to put the tensors on the GPU or not. We use the `torch.cuda.is_available()` function, which will return `True` if a GPU is detected on our computer. We pass this `device` to the iterator.
-# 
-# When we get a batch of examples using an iterator we need to make sure that all of the source sentences are padded to the same length, the same with the target sentences. Luckily, torchText iterators handle this for us! 
-# 
-# We use a `BucketIterator` instead of the standard `Iterator` as it creates batches in such a way that it minimizes the amount of padding in both the source and target sentences. 
-
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # device = torch.device('cpu')
@@ -131,25 +77,12 @@ print(torch.cuda.get_device_name(0))
 print(torch.rand(3,3).cuda())
 # input()
 
-
-
 BATCH_SIZE = 128
 
 train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
     (train_data, valid_data, test_data), 
     batch_size = BATCH_SIZE, 
     device = device)
-
-
-# # Training the Seq2Seq Model
-# 
-# Now we have our model implemented, we can begin training it. 
-# 
-# First, we'll initialize our model. As mentioned before, the input and output dimensions are defined by the size of the vocabulary. The embedding dimesions and dropout for the encoder and decoder can be different, but the number of layers and the size of the hidden/cell states must be the same. 
-# 
-# We then define the encoder, decoder and then our Seq2Seq model, which we place on the `device`.
-
-
 
 INPUT_DIM = len(SRC.vocab)
 OUTPUT_DIM = len(TRG.vocab)
@@ -165,13 +98,6 @@ dec = Decoder(OUTPUT_DIM, DEC_EMB_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT)
 
 model = Seq2Seq(enc, dec, device).to(device)
 
-
-# Next up is initializing the weights of our model. In the paper they state they initialize all weights from a uniform distribution between -0.08 and +0.08, i.e. $\mathcal{U}(-0.08, 0.08)$.
-# 
-# We initialize weights in PyTorch by creating a function which we `apply` to our model. When using `apply`, the `init_weights` function will be called on every module and sub-module within our model. For each module we loop through all of the parameters and sample them from a uniform distribution with `nn.init.uniform_`.
-
-
-
 def init_weights(m):
     for name, param in m.named_parameters():
         nn.init.uniform_(param.data, -0.08, 0.08)
@@ -179,7 +105,6 @@ def init_weights(m):
 model.apply(init_weights)
 
 
-# We also define a function that will calculate the number of trainable parameters in the model.
 
 
 
@@ -189,56 +114,18 @@ def count_parameters(model):
 print(f'The model has {count_parameters(model):,} trainable parameters')
 
 
-# We define our optimizer, which we use to update our parameters in the training loop. Check out [this](http://ruder.io/optimizing-gradient-descent/) post for information about different optimizers. Here, we'll use Adam.
 
 
 
 optimizer = optim.Adam(model.parameters())
 
 
-# Next, we define our loss function. The `CrossEntropyLoss` function calculates both the log softmax as well as the negative log-likelihood of our predictions. 
-# 
-# Our loss function calculates the average loss per token, however by passing the index of the `<pad>` token as the `ignore_index` argument we ignore the loss whenever the target token is a padding token. 
 
 
 
 TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
 
 criterion = nn.CrossEntropyLoss(ignore_index = TRG_PAD_IDX)
-
-
-# Next, we'll define our training loop. 
-# 
-# First, we'll set the model into "training mode" with `model.train()`. This will turn on dropout (and batch normalization, which we aren't using) and then iterate through our data iterator.
-# 
-# As stated before, our decoder loop starts at 1, not 0. This means the 0th element of our `outputs` tensor remains all zeros. So our `trg` and `outputs` look something like:
-# 
-# $$\begin{align*}
-# \text{trg} = [<sos>, &y_1, y_2, y_3, <eos>]\\
-# \text{outputs} = [0, &\hat{y}_1, \hat{y}_2, \hat{y}_3, <eos>]
-# \end{align*}$$
-# 
-# Here, when we calculate the loss, we cut off the first element of each tensor to get:
-# 
-# $$\begin{align*}
-# \text{trg} = [&y_1, y_2, y_3, <eos>]\\
-# \text{outputs} = [&\hat{y}_1, \hat{y}_2, \hat{y}_3, <eos>]
-# \end{align*}$$
-# 
-# At each iteration:
-# - get the source and target sentences from the batch, $X$ and $Y$
-# - zero the gradients calculated from the last batch
-# - feed the source and target into the model to get the output, $\hat{Y}$
-# - as the loss function only works on 2d inputs with 1d targets we need to flatten each of them with `.view`
-#     - we slice off the first column of the output and target tensors as mentioned above
-# - calculate the gradients with `loss.backward()`
-# - clip the gradients to prevent them from exploding (a common issue in RNNs)
-# - update the parameters of our model by doing an optimizer step
-# - sum the loss value to a running total
-# 
-# Finally, we return the loss that is averaged over all batches.
-
-
 
 def train(model, iterator, optimizer, criterion, clip):
     
@@ -278,17 +165,6 @@ def train(model, iterator, optimizer, criterion, clip):
         
     return epoch_loss / len(iterator)
 
-
-# Our evaluation loop is similar to our training loop, however as we aren't updating any parameters we don't need to pass an optimizer or a clip value.
-# 
-# We must remember to set the model to evaluation mode with `model.eval()`. This will turn off dropout (and batch normalization, if used).
-# 
-# We use the `with torch.no_grad()` block to ensure no gradients are calculated within the block. This reduces memory consumption and speeds things up. 
-# 
-# The iteration loop is similar (without the parameter updates), however we must ensure we turn teacher forcing off for evaluation. This will cause the model to only use it's own predictions to make further predictions within a sentence, which mirrors how it would be used in deployment.
-
-
-
 def evaluate(model, iterator, criterion):
     
     model.eval()
@@ -321,25 +197,11 @@ def evaluate(model, iterator, criterion):
         
     return epoch_loss / len(iterator)
 
-
-# Next, we'll create a function that we'll use to tell us how long an epoch takes.
-
-
-
 def epoch_time(start_time, end_time):
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
-
-
-# We can finally start training our model!
-# 
-# At each epoch, we'll be checking if our model has achieved the best validation loss so far. If it has, we'll update our best validation loss and save the parameters of our model (called `state_dict` in PyTorch). Then, when we come to test our model, we'll use the saved parameters used to achieve the best validation loss. 
-# 
-# We'll be printing out both the loss and the perplexity at each epoch. It is easier to see a change in perplexity than a change in loss as the numbers are much bigger.
-
-
 
 N_EPOCHS = 1
 CLIP = 1
